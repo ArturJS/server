68cbdd2313a43b0c6c567d6f2295726d08c5e74e
Lucas Löffel <lucas@loeffel.io>
2019-12-02 11:55:12 +0100

    unzip

--- go.mod
+++ go.mod
@@ -3,12 +3,6 @@ module github.com/loeffel-io/serve-server
 go 1.12
 
 require (
-	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v1.13.1
-	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gin-gonic/gin v1.5.0
-	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/net v0.0.0-20191126235420-ef20fe5d7933 // indirect
+	github.com/mholt/archiver/v3 v3.3.0
 )
--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mholt/archiver/v3"
 )
 
 func main() {
+	zip := archiver.Zip{}
 	router := gin.Default()
-	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.MaxMultipartMemory = 32 << 20 // 8 MiB
 	router.POST("/deploy", func(c *gin.Context) {
+		timestamp := time.Now().Unix()
+		service := c.PostForm("service")
+		zipName := fmt.Sprintf("%s-%d.zip", service, timestamp)
+
 		file, err := c.FormFile("file")
 
 		if err != nil {
@@ -20,13 +27,36 @@ func main() {
 			return
 		}
 
+		// save file
 		err = c.SaveUploadedFile(
 			file,
-			fmt.Sprintf("/home/serve/deployments/%d", time.Now().Unix()),
+			fmt.Sprintf("/home/serve/deployments/%s", zipName),
 		)
 
 		if err != nil {
-			c.JSON(http.StatusBadRequest, err)
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		// create build dir
+		err = os.MkdirAll(
+			fmt.Sprintf("/home/serve/builds/%s/%d", service, timestamp),
+			os.ModePerm,
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		// unzip to build dir
+		err = zip.Unarchive(
+			fmt.Sprintf("/home/serve/deployments/%s", zipName),
+			fmt.Sprintf("/home/serve/builds/%s/%d", service, timestamp),
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
 			return
 		}
 
