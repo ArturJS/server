b133d067e48d47a8dede9cbbbb42e0d43a963e1f
Lucas Löffel <lucas@loeffel.io>
2020-02-26 13:00:49 +0100

    update output

--- api.go
+++ api.go
@@ -292,7 +292,7 @@ func (api *api) deploy(c *gin.Context) {
 	out, err := dockerCmd.CombinedOutput()
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, api.response(err.Error(), out))
+		c.JSON(http.StatusInternalServerError, api.response(fmt.Errorf("%s: %s", err.Error(), out), nil))
 		return
 	}
 
