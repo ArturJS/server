3d06a89c031c977399304cdc499a6a2a26b8120f
Lucas Löffel <lucas@loeffel.io>
2020-02-26 12:49:10 +0100

    add combined output

--- api.go
+++ api.go
@@ -289,7 +289,7 @@ func (api *api) deploy(c *gin.Context) {
 	dockerCmd.Dir = symlink
 
 	// output
-	out, err := dockerCmd.Output()
+	out, err := dockerCmd.CombinedOutput()
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, api.response(err.Error(), out))
